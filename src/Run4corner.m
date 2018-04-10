14a556dd6f68b6c0a6352f88cd3fa7550b0d965c
torees <torees@stud.ntnu.no>
2018-04-10 14:48:39 +0200

    ned plotting script

--- src/Run4corner.m
+++ src/Run4corner.m
@@ -6,8 +6,8 @@ eta_initial = [5; -1; 0];
 nu_initial = [0; 0; 0];
 
 %Simulation parameters
-t_sim = 800;
-controller = 2; %L1 = 1, II = 2, 
+t_sim = 250;
+controller = 1; %L1 = 1, II = 2, 
 sim('Adaptive_control_CSAD_model');
 
 

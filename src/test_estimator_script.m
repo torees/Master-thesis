db207676bc006d3d2a616f8f1c00fda4919a4da7
torees <torees@stud.ntnu.no>
2018-04-18 19:01:17 +0200

    added chapter on Mclab finding

--- src/test_estimator_script.m
+++ src/test_estimator_script.m
@@ -14,11 +14,11 @@ eta_t=eta_t';
 sim('Mclab_estimator_fix')
 
 nu_hat = nu_hat';
-figure(7)
-hold on
-plot(nu_ts.Time,nu_ts.Data(:,2))
-plot(nu_hat.Time,nu_hat.Data(:,2))
-legend('gammel estimator','ny estimator','Location','best')
+% figure(7)
+% hold on
+% plot(nu_ts.Time,nu_ts.Data(:,2))
+% plot(nu_hat.Time,nu_hat.Data(:,2))
+% legend('gammel estimator','ny estimator','Location','best')
 
 
 
@@ -41,8 +41,8 @@ sim('Mclab_estimator_fix')
 nu_hat = nu_hat';
 figure(8)
 hold on
-plot(nu_ts.Time,nu_ts.Data(:,2))
-plot(nu_hat.Time,nu_hat.Data(:,2))
+plot(nu_ts.Time,nu_ts.Data(:,1))
+plot(nu_hat.Time,nu_hat.Data(:,1))
 legend('gammel estimator','ny estimator','Location','best')
 
 

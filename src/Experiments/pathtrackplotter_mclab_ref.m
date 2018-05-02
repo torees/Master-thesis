cb58eba0f6c9c441bf4490e397ced58b867899f7
torees <torees@stud.ntnu.no>
2018-05-02 16:42:55 +0200

    plotted L2 gain update and added to report

---
+++ src/Experiments/pathtrackplotter_mclab_ref.m
@@ -0,0 +1,24 @@
+function pathtrackplotter_mclab_ref(eta, tsamp, dec,color1,color2)
+
+eta_plot = eta';
+
+x = eta_plot(:,1);
+y = eta_plot(:,2);
+psi = eta_plot(:,3);
+
+plot(y,x,color2,'LineWidth',1.5)
+L=200/6;
+tnow=0;
+
+for now=1:dec:length(x)
+   
+    R = [cos(psi(now)) -sin(psi(now)); sin(psi(now)) cos(psi(now))];
+    boat = R/50*[6*L/9 5*L/9 3*L/9 -5*L/9 -5*L/9 3*L/9 5*L/9 6*L/9;
+                0 1*L/9 2*L/9 2*L/9 -2*L/9 -2*L/9 -1*L/9 0];
+
+    plot(y(now),x(now),color1,'LineWidth',1.5)
+    
+    tnow=tnow+tsamp*dec;
+
+end
+end
